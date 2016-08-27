bafc2c5134c401b291ebb0fb897ab9f310ceb423
Tony Crisci <tony@dubstepdish.com>
2016-08-26 23:25:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ansible Aptly Ops
+
+**Work in progress**
+
+Scripts to manage Debian repos and mirrors created with [Aptly](https://www.aptly.info/).
+
+This project is based on the [ansible-aptly-role](https://github.com/alexey-sveshnikov/ansible-aptly-role) but as a standalone project with greater scope.
+
+## Todo
+
+* better key management with vault
+* mirrors
+* replication
+* command line utility
+
+## License
+
+This work is licensed under a BSD license (see LICENSE)
+
+© 2016 Tony Crisci
---
+++ gen-key.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# A hacky script to generate a gpg key and include it in the project
+# ** NOT FOR PRODUCTION USE **
+
+cd "$(dirname "$0")"
+
+HAVEGED=`command -v haveged`
+
+if [ x$HAVEGED != x ]; then
+    echo "starting haveged service (requires root)"
+    sudo systemctl start haveged
+fi
+
+GPG_OUTPUT=$(mktemp)
+
+gpg --gen-key | tee $GPG_OUTPUT
+
+KEY=`grep ^pub $GPG_OUTPUT | head -n 1 | awk '{ print $2 }' | sed 's/[0-9a-zA-Z]\+\///'`
+
+echo "Using key: $KEY"
+
+mkdir -p secrets/aptly
+
+gpg --export-secret-keys --armor $KEY > secrets/aptly/private.key
+gpg --export --armor $KEY > secrets/aptly/public.key
+
+# XXX very hacky way to setting this variable
+# TODO dynamically get this variable within the ansible role with `gpg $KEY` to
+# get info
+perl -pi -e "s/^aptly_secret_key_id:.*/aptly_secret_key_id: $KEY/g" group_vars/all
+
+if [ x$HAVEGED != x ]; then
+    echo "stopping havaged service (requires root)"
+fi
